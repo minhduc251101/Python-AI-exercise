841aaedc58ad9b6042ce32287a5975fa3af16fa7
Danny <114160317+NhatAnh1708@users.noreply.github.com>
2026-03-01 08:57:38 +0000

    update the logic of regex ip

--- regex_email.py
+++ regex_email.py
@@ -1,16 +1,55 @@
 import re
-
+from typing import Any
+from enum import Enum
 
 # 9.1) Kiểm tra địa chỉ IPv4 hợp lệ (x.x.x.x, x trong [0, 255])
-def check_ipv4(ip):
+def check_ipv4(ip) -> bool:
     pattern = r'^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$'
-    return bool(re.match(pattern, ip))
+    return re.match(pattern, ip)
 
-print("=== 9.1 IPv4 ===")
-print(check_ipv4("192.168.1.1"))   # True
-print(check_ipv4("256.0.0.1"))     # False
-print(check_ipv4("0.0.0.0"))       # True
-print(check_ipv4("999.999.999.999")) # False
+class IP_TYPE(Enum):
+    V4 = "v4"
+    V6 = "v6"
+
+
+## basic pattern -> struct
+def check_ip(ip: str, ip_type: IP_TYPE = IP_TYPE) -> bool:
+    if isinstance(ip, int):
+        raise ValueError("must be define the type")
+    if ip_type == IP_TYPE.V4.value:
+        print("USING V4")
+        pattern = r'^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$'
+        if re.match(pattern, ip):
+            print("This is a ipv4")
+            return True # early return
+        print("this isn't a ipv4")
+        return False
+    elif ip_type == IP_TYPE.V6.value:
+        print("USING V6")
+        #TODO: implement define the pattern of IPv6
+        return True
+    print("code sai")
+    return False
+
+class CHECKIP: # --> 
+    def __init__(self, ip_type: IP_TYPE): # --> 
+        self.ip_type = ip_type
+        self.pattern_v4 = r'^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$'
+
+    def check_ip_type_basic(self): 
+        return self.ip_type
+    
+    @property
+    def check_ip_type(self): 
+        return self.ip_type
+    # pattern = r'^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$'
+    # return bool(re.match(pattern, ip))
+
+# print("=== 9.1 IPv4 ===")
+# print(check_ipv4("192.168.1.1"))   # True
+# print(check_ipv4("256.0.0.1"))     # False
+# print(check_ipv4("0.0.0.0"))       # True
+# print(check_ipv4("999.999.999.999")) # False
 
 
 # 9.2) Trích xuất tất cả email trong văn bản
@@ -18,9 +57,9 @@ def extract_emails(text):
     pattern = r'[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}'
     return re.findall(pattern, text)
 
-print("\n9.2 Extract Emails ")
-text = "Liên hệ: admin@gmail.com, support@company.org hoặc user.name+tag@sub.domain.vn"
-print(extract_emails(text))
+# print("\n9.2 Extract Emails ")
+# text = "Liên hệ: admin@gmail.com, support@company.org hoặc user.name+tag@sub.domain.vn"
+# print(extract_emails(text))
 
 
 # 9.3) Kiểm tra chuỗi khác rỗng, chỉ chứa a-z, A-Z, 0-9
@@ -28,11 +67,11 @@ def check_alphanumeric(s):
     pattern = r'^[a-zA-Z0-9]+$'
     return bool(re.match(pattern, s))
 
-print("\n=== 9.3 Alphanumeric ===")
-print(check_alphanumeric("Hello123"))  # True
-print(check_alphanumeric(""))          # False
-print(check_alphanumeric("Hello!"))    # False
-print(check_alphanumeric("abc_123"))   # False
+# print("\n=== 9.3 Alphanumeric ===")
+# print(check_alphanumeric("Hello123"))  # True
+# print(check_alphanumeric(""))          # False
+# print(check_alphanumeric("Hello!"))    # False
+# print(check_alphanumeric("abc_123"))   # False
 
 
 # 9.4) Kiểm tra độ mạnh mật khẩu
@@ -52,9 +91,18 @@ def check_password(pwd):
     ]
     return all(re.search(p, pwd) for p in checks)
 
-print("\n=== 9.4 Password Strength ===")
-print(check_password("Abc@1234"))     # True
-print(check_password("abc@1234"))     # False (thiếu chữ hoa)
-print(check_password("Abcdefgh"))     # False (thiếu số và ký tự đặc biệt)
-print(check_password("Ab@1"))         # False (quá ngắn)
-print(check_password("Abc@12345"))    # True
+# print("\n=== 9.4 Password Strength ===")
+# print(check_password("Abc@1234"))     # True
+# print(check_password("abc@1234"))     # False (thiếu chữ hoa)
+# print(check_password("Abcdefgh"))     # False (thiếu số và ký tự đặc biệt)
+# print(check_password("Ab@1"))         # False (quá ngắn)
+# print(check_password("Abc@12345"))    # True
+
+
+if __name__ == "__main__":
+    input_ip = "192.168.1.1.1"
+    # print(check_ipv4(input_ip)) ## true
+    # print('*'*40)
+    # print(check_ip(input_ip, ip_type="v4")) ## true
+    checkip = CHECKIP(ip_type=IP_TYPE.V4.value)
+    print(checkip.check_ip_type)
