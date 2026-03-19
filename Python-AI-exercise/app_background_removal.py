dec57c888dd181589951a99352974024dd403676
minhduc251101 <minhducnvan2511@gmail.com>
2026-03-19 18:31:32 +1100

    FE and Upload background removal gui

---
+++ Python-AI-exercise/app_background_removal.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from rembg import remove
+import io
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # đổi thành origin FE khi deploy
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    input_bytes = await file.read()
+    output_bytes = remove(input_bytes, force_return_bytes=True)
+
+    return StreamingResponse(
+        io.BytesIO(output_bytes),
+        media_type="image/png",
+        headers={"Content-Disposition": "inline; filename=output.png"}
+    )
--- Python-AI-exercise/background_removal.py
+++ Python-AI-exercise/background_removal.py
@@ -1,5 +1,5 @@
 from rembg import remove
-
+# input_path = Path("images") / "vecteezy_smartphone-and-mobile-phone_11047522.png"
 with open('/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/Python-AI-exercise/vecteezy_smartphone-and-mobile-phone_11047522.png',"rb") as i:
     with open('output1.png', 'wb') as o:
         input = i.read()
