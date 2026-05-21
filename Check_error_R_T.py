d7f5dfca1ec3c2b337f8efacd800261febe773ee
minhduc251101 <minhducnvan2511@gmail.com>
2026-05-21 16:38:42 +1000

    update check error RSME funcction

---
+++ Check_error_R_T.py
@@ -0,0 +1,25 @@
+import numpy as np
+def get_angular_error(R_exp, R_est):
+    """
+    Calculate angular error
+    """
+    return abs(np.arccos(min(max(((np.matmul(R_exp.T, R_est)).trace() - 1) / 2, -1.0), 1.0)))
+def pose_error(T_est, T_gt):
+    T_err = T_gt @ np.linalg.inv(T_est)
+    trans_error = np.linalg.norm(T_err[:3, 3])
+    R_err = T_err[:3, :3]
+    rot_error_rad = (get_angular_error(T_gt[:3, :3], T_est[:3, :3]))
+    rot_error_deg  = np.rad2deg(rot_error_rad)
+    return trans_error, rot_error_deg
+
+T_gt = np.loadtxt('/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/gt_transform.txt')
+T_est = np.loadtxt('/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/estimated_transform_final.txt')
+# T_est = np.loadtxt('/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/go_icp_output.txt')
+
+trans_error, rot_error_deg = pose_error(T_est, T_gt)
+print("\n\nFPFH + RANSAC:\n\n")
+# print("\n\nGO-ICP:\n\n")
+print("T_gt =\n", T_gt)
+print("T_est =\n", T_est)
+print("translation error =", trans_error)
+print("rotation error (deg) =", rot_error_deg)
--- pcd_to_xyz.py
+++ pcd_to_xyz.py
@@ -1,11 +1,23 @@
 import open3d as o3d
+import os
+import shutil
 
 # 1. Load the PLY file
 # Replace 'input.ply' with the path to your file
-pcd = o3d.io.read_point_cloud("/media/minhduc/TOSHIBA EXT1/4dradarslam/NTU4Dradlm/cp/cp/loop_true_fullpose/005032/cloud.pcd")
+pcd = o3d.io.read_point_cloud("/media/minhduc/TOSHIBA EXT1/4dradarslam/NTU4Dradlm/cp/cp/loop_true_fullpose/000027/cloud.pcd")
 
 # 2. Save the point cloud as an XYZ file
 # Open3D automatically formats it as space-separated coordinates
-o3d.io.write_point_cloud("/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/output_005032.xyz", pcd)
+o3d.io.write_point_cloud("/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/output_000027.xyz", pcd)
 
 print("Conversion complete!")
+
+# The path to your existing XYZ file
+source_file = "/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/output_000027.xyz"
+# The path for your new TXT file
+destination_file = "/media/minhduc/TOSHIBA EXT1/C++ hoc/Python-AI-exercise/results_27_5032/output_000027.txt"
+
+# This perfectly copies the file and gives it the new extension
+shutil.copy(source_file, destination_file)
+
+print(f"Successfully saved as '{destination_file}'")
